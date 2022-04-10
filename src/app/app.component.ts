d1f00bb795652fc6e543460baaa9066fffd00d7f
kuria-wachira25 <kuria.wachira25@gmail.com>
2022-04-10 21:01:55 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+type Response = {
+  status: string;
+  content: {
+    _id: string;
+    name: string;
+    size: number;
+    md5: string;
+    mimetype: string;
+    s3Ref: string;
+    filePath: string;
+    uploadedAt: string;
+    extension: string;
+    url: string;
+  };
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  imageUrl: string | null = null;
+  imageName: string | null = null;
+  loading: boolean = false;
+  errorMessage: Error | null = null;
+
+  constructor(private httpClient: HttpClient) {
+    this.getImage();
+  }
+
+  public async getImage() {
+    this.httpClient.get(`${environment.apiEndpoint}/image`).subscribe(
+      (response) => {
+        const {
+          content: { url, name },
+        } = response as Response;
+        this.imageUrl = url;
+        this.imageName = name;
+      },
+      (error) => {
+        this.errorMessage = error.message;
+        this.loading = false;
+      },
+      () => {
+        this.loading = false;
+      }
+    );
+  }
+}
