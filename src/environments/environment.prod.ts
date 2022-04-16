c211d541c51cab6ca9a05210a814e2bac19220ad
kuria-wachira25 <kuria.wachira25@gmail.com>
2022-04-16 22:03:15 +0300

    Updated prod api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiEndpoint:
-    'http://ace7df1d4c08d46608a980169e3a7d08-501520679.us-west-1.elb.amazonaws.com',
+    'http://a53d4e0ab226a4de585579089afbc8bd-822228348.us-west-1.elb.amazonaws.com',
 };
